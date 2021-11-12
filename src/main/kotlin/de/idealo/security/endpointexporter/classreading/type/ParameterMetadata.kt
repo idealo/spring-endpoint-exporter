83b7df2b043835ab900bb4b5cddc42fb4a71e5b6
Tommy Schmidt <tommy.schmidt@idealo.de>
2021-11-12 17:20:09 +0100

    ci: add build workflow, add PR checks, setup sonarcloud integration (#4)

--- src/main/kotlin/de/idealo/security/endpointexporter/classreading/type/ParameterMetadata.kt
+++ src/main/kotlin/de/idealo/security/endpointexporter/classreading/type/ParameterMetadata.kt
@@ -2,31 +2,28 @@ package de.idealo.security.endpointexporter.classreading.type
 
 data class ParameterMetadata(
     val name: String,
-    val access: Int,
     val type: String,
     private val annotations: List<AnnotationMetadata>
 ) : AnnotatedTypeMetadata {
 
-	class Builder(
+    class Builder(
         var name: String? = null,
-        var access: Int? = null,
         var type: String? = null,
         private val annotations: MutableList<AnnotationMetadata> = mutableListOf()
-	) {
+    ) {
 
         fun addAnnotation(annotation: AnnotationMetadata) {
             annotations.add(annotation)
         }
 
-		fun build(): ParameterMetadata {
-			return ParameterMetadata(
-				name = name!!,
-				access = access!!,
-				type = type!!,
+        fun build(): ParameterMetadata {
+            return ParameterMetadata(
+                name = name!!,
+                type = type!!,
                 annotations = annotations
-			)
-		}
-	}
+            )
+        }
+    }
 
     override fun getAnnotations(): List<AnnotationMetadata> = annotations
 }
