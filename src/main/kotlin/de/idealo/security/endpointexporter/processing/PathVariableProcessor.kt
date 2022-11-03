f66efa51a05d65b334de388a6d34974d30eace78
Tommy Schmidt <tommy.schmidt@idealo.de>
2022-11-03 11:55:12 +0100

    chore: cleanup PathVariableProcessor

--- src/main/kotlin/de/idealo/security/endpointexporter/processing/PathVariableProcessor.kt
+++ src/main/kotlin/de/idealo/security/endpointexporter/processing/PathVariableProcessor.kt
@@ -3,21 +3,20 @@ package de.idealo.security.endpointexporter.processing
 import de.idealo.security.endpointexporter.classreading.type.AnnotationMetadata
 import de.idealo.security.endpointexporter.classreading.type.MethodMetadata
 import de.idealo.security.endpointexporter.classreading.type.ParameterMetadata
-import de.idealo.security.endpointexporter.processing.RequestMapping.PathVariable
 import org.springframework.stereotype.Component
-import org.springframework.web.bind.annotation.PathVariable as PathVariableAnnotation
+import org.springframework.web.bind.annotation.PathVariable
 
 @Component
-class PathVariableProcessor : MetadataProcessor<MethodMetadata, List<PathVariable>> {
+class PathVariableProcessor : MetadataProcessor<MethodMetadata, List<RequestMapping.PathVariable>> {
 
-    override fun process(metadata: MethodMetadata): List<PathVariable> {
+    override fun process(metadata: MethodMetadata): List<RequestMapping.PathVariable> {
 
         return metadata.parameters
-            .filter { it.isAnnotated(PathVariableAnnotation::class.qualifiedName!!) }
+            .filter { it.isAnnotated(PathVariable::class.qualifiedName!!) }
             .map { parameterMetadata ->
-                val parameterAnnotationAttributes = parameterMetadata.getAnnotation(PathVariableAnnotation::class.qualifiedName!!)!!
+                val parameterAnnotationAttributes = parameterMetadata.getAnnotation(PathVariable::class.qualifiedName!!)!!
 
-                PathVariable(
+                RequestMapping.PathVariable(
                     name = getParameterName(parameterMetadata, parameterAnnotationAttributes),
                     type = parameterMetadata.type,
                     required = parameterAnnotationAttributes.getBoolean("required") ?: true
